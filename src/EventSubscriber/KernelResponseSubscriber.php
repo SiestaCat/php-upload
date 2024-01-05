03089e3724ee98682e256f2efc1b614f4e157507
Marco <marco.isfc@gmail.com>
2024-01-05 01:18:45 +0100

    add cors

---
+++ src/EventSubscriber/KernelResponseSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class KernelResponseSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        ];
+    }
+
+    public function onKernelResponse(ResponseEvent $event):void
+    {
+        if($event->getRequest()->getMethod() === 'OPTIONS')
+        {
+            $event->setResponse
+            (
+                new Response
+                (
+                    null,
+                    204,
+                    [
+                        'Access-Control-Allow-Origin' => '*'
+                    ]
+                )
+            );
+        }
+    }
+}
